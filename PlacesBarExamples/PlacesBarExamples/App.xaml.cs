b3852b2a4c41271c5e1dab16a568ac6f27b1761c
John Lanz <lanzwjohn@gmail.com>
2018-07-31 17:35:36 -0400

    Finish converting to .netstandard and adding in XAML binding support for mvvm

--- PlacesBarExamples/PlacesBarExamples/App.xaml.cs
+++ PlacesBarExamples/PlacesBarExamples/App.xaml.cs
@@ -1,27 +1,30 @@
-﻿using Xamarin.Forms;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
 namespace PlacesBarExamples
 {
 	public partial class App : Application
 	{
-		public App()
+		public App ()
 		{
 			InitializeComponent();
 
-			MainPage = new PlacesBarExamplesPage();
-		}
+            MainPage = new PlacesBarExamplesPage();
+        }
 
-		protected override void OnStart()
+		protected override void OnStart ()
 		{
 			// Handle when your app starts
 		}
 
-		protected override void OnSleep()
+		protected override void OnSleep ()
 		{
 			// Handle when your app sleeps
 		}
 
-		protected override void OnResume()
+		protected override void OnResume ()
 		{
 			// Handle when your app resumes
 		}
