8ab4eeeda625929267391a41967eea288a04d886
Mike Schwörer <mailport@mikescher.de>
2019-06-10 22:12:58 +0200

    Get values of "terms" and "types" from autocomplete prediction result

--- PlacesSearchBar/AutoCompletePrediction.cs
+++ PlacesSearchBar/AutoCompletePrediction.cs
@@ -24,6 +24,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -70,16 +72,33 @@ namespace DurianCode.PlacesSearchBar
 		/// <value>The secondary text.</value>
 		public string SecondaryText { get; set; }
 
+		/// <summary>
+		/// Gets the individual terms of this prediction
+		/// </summary>
+		/// <value>The terms.</value>
+		public List<string> Terms { get; set; }
+
+		/// <summary>
+		/// Gets the types of this prediction
+		/// see https://developers.google.com/places/web-service/supported_types
+		/// </summary>
+		/// <value>The types of the prediction.</value>
+		public List<string> Types { get; set; }
+
 		public static AutoCompletePrediction FromJson(JObject json)
 		{
-			var r = new AutoCompletePrediction();
+			var r = new AutoCompletePrediction
+			{
+				Description   = json["description"].Value<string>(),
+				ID            = json["id"].Value<string>(),
+				Place_ID      = json["place_id"].Value<string>(),
+				Reference     = json["reference"].Value<string>(),
+				MainText      = json["structured_formatting"]["main_text"].Value<string>(),
+				SecondaryText = json["structured_formatting"]["secondary_text"].Value<string>(),
+				Terms         = json["terms"].Value<JArray>().Select(p => p["value"].Value<string>()).ToList(),
+				Types         = json["types"].Value<JArray>().Select(p => p.Value<string>()).ToList()
+			};
 
-			r.Description   = json["description"].Value<string>();
-			r.ID            = json["id"].Value<string>();
-			r.Place_ID      = json["place_id"].Value<string>();
-			r.Reference     = json["reference"].Value<string>();
-			r.MainText      = json["structured_formatting"]["main_text"].Value<string>();
-			r.SecondaryText = json["structured_formatting"]["secondary_text"].Value<string>();
 
 			return r;
 		}
